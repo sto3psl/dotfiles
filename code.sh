7fd97207f9c6a5586246cb5764ba0340a9af9cf5
Fabian Gündel <f.guendel@gmail.com>
2017-10-02 18:19:44 +0200

    remove unused stuff

---
+++ code.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Install Visual Studio Code plugins
+code --install-extension teabyii.ayu
+code --install-extension PKief.material-icon-theme
+code --install-extension TimonVS.ReactSnippetsStandard
+code --install-extension chenxsan.vscode-standard-format
+code --install-extension chenxsan.vscode-standardjs
+code --install-extension christian-kohler.npm-intellisense
+code --install-extension christian-kohler.path-intellisense
+code --install-extension dsznajder.es7-react-js-snippets
+code --install-extension dzannotti.vscode-babel-coloring
+code --install-extension esbenp.prettier-vscode
+code --install-extension ms-vscode.sublime-keybindings
+code --install-extension robertohuertasm.vscode-icons
+code --install-extension shinnn.stylelint
+code --install-extension xabikos.JavaScriptSnippets
